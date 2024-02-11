575a2940530d21ee039ce07ae5ee53c52ab3b943
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-11 17:59:14 +0530

    Refactor EditTopic component and add getTopicById function

--- app/edit-topic/[id]/page.jsx
+++ app/edit-topic/[id]/page.jsx
@@ -3,13 +3,30 @@ import EditTopic from "@/components/edit-topic/EditTopic";
 export const metadata = {
   title: "Edit Topic | HawtCRUD",
 };
-//desc: edit topic page
-const page = () => {
+
+const getTopicById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to get topic");
+    }
+    return await res.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const EditTopicPage = async ({ params }) => {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
   return (
     <>
-      <EditTopic />
+      <EditTopic id={id} title={title} description={description} />
     </>
   );
 };
 
-export default page;
+export default EditTopicPage;
--- components/edit-topic/EditTopic.jsx
+++ components/edit-topic/EditTopic.jsx
@@ -1,26 +1,58 @@
+"use client";
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
-const EditTopic = () => {
+const EditTopic = ({ id, title, description }) => {
+  const route = useRouter();
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+
+  const handelOnSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to Edit topic");
+      }
+      alert("Updated successfully");
+      route.push("/");
+      route.refresh();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-      <form className="flex flex-col gap-3">
-        <input
-          type="text"
-          name="topic"
-          id="topic"
-          placeholder="Topic name"
-          className="border border-slate-600 outline-none w-full p-4 text-lg rounded"
-        />
-        <input
-          type="text"
-          name="desc"
-          id="desc"
-          placeholder="Description"
-          className="border border-slate-600 outline-none w-full p-4 text-lg rounded"
-        />
-        <Button variant="outline" className="text-lg font-bold w-fit">
-          Edit topic
-        </Button>
-      </form>
+    <form onSubmit={handelOnSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        type="text"
+        name="topic"
+        id="topic"
+        placeholder="Topic name"
+        className="border border-slate-600 outline-none w-full p-4 text-lg rounded"
+      />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        type="text"
+        name="desc"
+        id="desc"
+        placeholder="Description"
+        className="border border-slate-600 outline-none w-full p-4 text-lg rounded"
+      />
+      <Button type={`submit`} variant="outline" className="text-lg font-bold w-fit">
+        Edit topic
+      </Button>
+    </form>
   );
 };
 
