0105be6389fed93647aa9099af11ad8f32b1788e
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-11 15:12:29 +0530

    Refactor TopicList component to fetch and display topics dynamically

--- components/topic-list/TopicList.jsx
+++ components/topic-list/TopicList.jsx
@@ -2,24 +2,46 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { Pencil, Trash2 } from "lucide-react";
 
-const TopicList = () => {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) throw new Error("Error fetching topics");
+    return await res.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const TopicList = async () => {
+  const { topics } = await getTopics();
   return (
-    <div className="border border-slate-600 px-2 flex justify-between gap-3 mb-2 py-5">
-      <div>
-        <h2 className="text-lg font-bold">Topic Title</h2>
-        <h2>Topic Description</h2>
-      </div>
-      <div className="flex justify-between items-center">
-        <Link href={`/edit-topic/123`}>
-          <Button variant="outline" size="icon" className="mr-2">
-            <Pencil className="w-4" />
-          </Button>
-        </Link>
-          <Button variant="outline" size="icon">
-            <Trash2 className="w-4 text-red-500" />
-          </Button>
-      </div>
-    </div>
+    <>
+      {topics.map((topic, index) => {
+        return (
+          <div
+            className="border border-slate-600 px-2 flex justify-between gap-3 mb-2 py-5"
+            key={index}
+          >
+            <div>
+              <h2 className="text-lg font-bold">{topic.title}</h2>
+              <h2>{topic.description}</h2>
+            </div>
+            <div className="flex justify-between items-center">
+              <Link href={`/edit-topic/${topic._id}`}>
+                <Button variant="outline" size="icon" className="mr-2">
+                  <Pencil className="w-4" />
+                </Button>
+              </Link>
+              <Button variant="outline" size="icon">
+                <Trash2 className="w-4 text-red-500" />
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
