d2cf24294fe5ca9a4332092a50a6137aad5959c6
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-11 11:02:42 +0530

    Add GET route to fetch topics

--- app/(backend)/api/topics/route.js
+++ app/(backend)/api/topics/route.js
@@ -7,3 +7,9 @@ export async function POST(req) {
   await Topic.create({ title, description });
   return Response.json({ message: "topic created" }, { status: 201 });
 }
+
+export async function GET(_req) {
+  await connectDB();
+  const topics = await Topic.find();
+  return Response.json({topics});
+}
