41c706b8ff4786587b442c2638b6dfa356711596
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-13 03:15:48 +0530

    Remove error message from Error component

--- app/error.jsx
+++ app/error.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const Error = ({ error, reset }) => {
   return (
     <div className="bg-red-500 text-white p-4 rounded flex items-center justify-between">
-      <p>Something went wrong!{error.message}</p>
+      <p>Something went wrong!</p>
       <Button onClick={reset}>Try again</Button>
     </div>
   );
