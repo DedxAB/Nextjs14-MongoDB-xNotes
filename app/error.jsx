18e9295a3a80dbeb31db13b42c9a40fc82382bf2
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-13 03:09:06 +0530

    Remove error message variable from JSX component

--- app/error.jsx
+++ app/error.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const Error = ({ error, reset }) => {
   return (
     <div className="bg-red-500 text-white p-4 rounded">
-      <p>Something went wrong!{error}</p>
+      <p>Something went wrong!</p>
       <Button onClick={reset}>Try again</Button>
     </div>
   );
