2090b3bbedb31e6273ee3503b725cf4ca747f6fd
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-12 13:05:38 +0530

    Refactor AddTopic component and update page.jsx

---
+++ components/add-topic/AddTopic.jsx
@@ -0,0 +1,74 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+const AddTopic = () => {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    if (!topic || !description) {
+      alert("Please fill all the fields");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to save topic");
+      }
+      alert("Topic saved successfully");
+      router.push("/");
+      router.refresh();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handelSubmit} className="flex flex-col gap-3">
+        <input
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          type="text"
+          name="topic"
+          id="topic"
+          placeholder="Topic name"
+          className="border shadow outline-none w-full p-4 text-lg rounded"
+        />
+        <input
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Description"
+          className="border shadow outline-none w-full p-4 text-lg rounded"
+        />
+        <Button
+          type={`submit`}
+          variant="outline"
+          className="text-lg font-bold w-[109px] rounded"
+        >
+          Save Topic
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default AddTopic;
