e39aacbae26b752750dbae5c705b1912a08e1ebc
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-13 02:39:11 +0530

    Add Error and NotFound components

---
+++ app/error.jsx
@@ -0,0 +1,10 @@
+"use client"
+const Error = () => {
+    return (
+        <div className="bg-red-500 text-white p-4 rounded">
+            <p>Something went wrong!</p>
+        </div>
+    );
+};
+
+export default Error;
---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+      <p className="text-lg">The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
