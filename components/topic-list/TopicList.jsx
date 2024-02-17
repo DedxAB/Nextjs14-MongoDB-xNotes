131bf698e1254062b7344f62aad1634838f828f7
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-18 01:39:48 +0530

    Refactor TopicList component to use useState and useEffect hooks

--- components/topic-list/TopicList.jsx
+++ components/topic-list/TopicList.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Pencil } from "lucide-react";
@@ -12,36 +15,51 @@ const getTopics = async () => {
     if (!res.ok) throw new Error("Error fetching topics");
     return await res.json();
   } catch (error) {
-    console.log(error.message);
+    return { error: error.message };
   }
 };
 
-const TopicList = async () => {
-  const { topics } = await getTopics();
+const TopicList = () => {
+  const [topics, setTopics] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+    const result = await getTopics();
+    if (result.error) {
+      setError(result.error);
+    } else {
+      setTopics(result.topics);
+    }
+    };
+    fetchTopics();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <>
-      {topics?.map((topic, index) => {
-        return (
-          <div
-            className="border flex justify-between gap-3 mb-3 rounded px-4 py-2 shadow cursor-pointer hover:shadow-lg transition-all duration-300 ease-in-out"
-            key={index}
-          >
-            <div>
-              <h2 className="text-lg font-bold underline">{topic?.title}</h2>
-              <h2 className="font-bold mt-2">{topic?.description}</h2>
-            </div>
-            <div className="flex justify-between items-center">
-              <Link href={`/edit-topic/${topic?._id}`}>
-                <Button variant="outline" size="icon" className="mr-2">
-                  <Pencil className="w-4" />
-                </Button>
-              </Link>
-              <RemoveBtn id={topic?._id} />
-            </div>
+      {topics.map((topic) => (
+        <div
+          className="border flex justify-between gap-3 mb-3 rounded px-4 py-2 shadow cursor-pointer hover:shadow-lg transition-all duration-300 ease-in-out"
+          key={topic._id}
+        >
+          <div>
+            <h2 className="text-lg font-bold underline">{topic.title}</h2>
+            <h2 className="font-bold mt-2">{topic.description}</h2>
           </div>
-        );
-      })}
+          <div className="flex justify-between items-center">
+            <Link href={`/edit-topic/${topic._id}`}>
+              <Button variant="outline" size="icon" className="mr-2">
+                <Pencil className="w-4" />
+              </Button>
+            </Link>
+            <RemoveBtn id={topic._id} />
+          </div>
+        </div>
+      ))}
     </>
   );
 };
