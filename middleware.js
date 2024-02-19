c4c1aa610a43f8687506a28947f530f63fc5ecf7
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-19 21:02:36 +0530

    Add new routes for topic addition and editing

--- middleware.js
+++ middleware.js
@@ -9,6 +9,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/add-topic",
+    "/edit-topic/:path*",
+    // "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
