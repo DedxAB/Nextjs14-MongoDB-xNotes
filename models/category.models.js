f117fa1b79a6e09a285acf54db68c5382005800f
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-25 17:32:04 +0530

    Refactor category schema in category.models.js

--- models/category.models.js
+++ models/category.models.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({}, { timestamps: true });
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    articles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Article",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 const Category =
   mongoose.models.Category || mongoose.model("Category", categorySchema);
 
