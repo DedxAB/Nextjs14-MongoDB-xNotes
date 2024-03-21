a51a622dee011bf79bda70b643c304010041319e
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-22 00:59:45 +0530

    Refactor MongoDB connection logic

--- helper/mongodb.js
+++ helper/mongodb.js
@@ -1,10 +1,28 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  /*
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {
+    throw new Error("MongoDB connection failed");
+  }
+  */
+  const connectionState = mongoose.connection.readyState;
+  if (connectionState === 1) {
+    console.log("Already connected");
+    return;
+  }
+  if (connectionState === 2) {
+    console.log("Connecting...");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("Error connecting to Database", error.message);
     throw new Error("MongoDB connection failed");
   }
 };
