f81b88aec039a733671bfde17eb5c94f1ea20e9b
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-22 21:42:01 +0530

    Update error button variant

--- app/error.jsx
+++ app/error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
   return (
     <div className="bg-red-500 text-white p-4 rounded flex items-center justify-between">
       <p>Something went wrong!</p>
-      <Button onClick={() => window.location.reload(false)}>
+      <Button variant={`outline`} onClick={() => window.location.reload(false)}>
         Reload again
       </Button>
     </div>
