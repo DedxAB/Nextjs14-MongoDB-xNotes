135c96cc66e7eef3200aad4961379b95a98221ea
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-22 22:37:47 +0530

    Add font-bold class to reload button

--- app/error.jsx
+++ app/error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
   return (
     <div className="p-4 rounded flex items-center justify-start gap-3 text-lg font-bold">
       <p>Something went wrong!</p>
-      <Button variant={`outline`} onClick={() => window.location.reload(false)}>
+      <Button variant={`outline`} onClick={() => window.location.reload(false)} className={`text-lg font-bold`}>
         Reload again
       </Button>
     </div>
