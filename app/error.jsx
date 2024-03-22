05e00105e7aac3791d3ee48f590c38a7e320e853
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-22 22:32:31 +0530

    Refactor error component styling

--- app/error.jsx
+++ app/error.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const Error = () => {
   return (
-    <div className="bg-red-500 text-white p-4 rounded flex items-center justify-between">
+    <div className="p-4 rounded flex items-center justify-start gap-3 text-lg font-bold">
       <p>Something went wrong!</p>
       <Button variant={`outline`} onClick={() => window.location.reload(false)}>
         Reload again
