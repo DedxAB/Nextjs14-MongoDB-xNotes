6b7fa6d82bcff338e7407c3429feba1af18b5cff
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-24 15:34:10 +0530

    Refactor error message and add gradient effect to text

--- app/error.jsx
+++ app/error.jsx
@@ -5,8 +5,17 @@ import { Button } from "@/components/ui/button";
 const Error = () => {
   return (
     <div className="p-4 rounded flex items-center justify-center gap-3 text-lg font-bold">
-      <p>Something went wrong!</p>
-      <Button variant={`outline`} onClick={() => window.location.reload(false)} className={`text-lg font-bold`}>
+      <p>
+        <span className="bg-gradient-to-r from-blue-500  via-red-500 to-pink-500 bg-clip-text text-transparent">
+          Opps!
+        </span>
+        Something went wrong!
+      </p>
+      <Button
+        variant={`outline`}
+        onClick={() => window.location.reload(false)}
+        className={`text-lg font-bold`}
+      >
         Retry
       </Button>
     </div>
