87742162b2bc344324d199f8c798a83407cad2c9
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-30 00:39:30 +0530

    Add tags and likes/comments section to NoteCard component

--- components/NoteCard/NoteCard.jsx
+++ components/NoteCard/NoteCard.jsx
@@ -118,7 +118,52 @@ const NoteCard = ({ id, note, user }) => {
           <div className="flex justify-between items-cente mt-2">
             <h2 className="text-sm font-bold py-1">{note?.description}</h2>
           </div>
-          <div></div>
+
+          {/* Show the tags */}
+          <div>
+            <span className="text-sm hover:underline">#nextjs</span>
+          </div>
+
+          {/* Show the likes and comments */}
+          <div className="flex gap-5 mt-3">
+            {/* Likes  */}
+            <div className="flex gap-1 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+              <span className="text-sm font-bold">200 Likes</span>
+            </div>
+
+            {/* Comments */}
+            <div className="flex gap-1 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+                />
+              </svg>
+              <span className="text-sm font-bold">5 Comments</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
