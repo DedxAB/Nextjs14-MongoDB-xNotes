b0914c9e73335368b911e2c4c89140b5085031d6
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-03-29 14:52:11 +0530

    Update UserBanner margin-top in UserBanner.jsx

--- components/UserBanner/UserBanner.jsx
+++ components/UserBanner/UserBanner.jsx
@@ -20,7 +20,7 @@ const UserBanner = async () => {
         </>
       ) : (
         <>
-          <div className="font-bold text-[#444746] mb-6 mt-3">
+          <div className="font-bold text-[#444746] mb-6 mt-8">
             <h1 className="text-4xl md:text-5xl bg-gradient-to-r from-blue-500  via-red-500 to-pink-500 bg-clip-text text-transparent">
               Hello, Guest
             </h1>
