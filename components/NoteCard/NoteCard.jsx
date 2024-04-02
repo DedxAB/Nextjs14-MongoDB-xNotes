ffadd5c88fb8c8ce915f5093c6c8eea193fd0aef
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-02 18:30:26 +0530

    Add like functionality to NoteCard component

--- components/NoteCard/NoteCard.jsx
+++ components/NoteCard/NoteCard.jsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import RemoveButton from "../RemoveButton/RemoveButton";
 import dayjs from "dayjs";
 import { Playfair_Display } from "next/font/google";
@@ -15,7 +15,27 @@ const playfair = Playfair_Display({
 
 const NoteCard = ({ note, user }) => {
   const { data: session } = useSession();
+  const router = useRouter();
   const pathName = usePathname();
+
+  const handelLike = async (isLiked) => {
+    try {
+      const res = await fetch(`/api/topics/${note?._id}/likes`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: session?.user?.id, isLiked }),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error("Failed to like the note" || errorData.message);
+      }
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const truncateString = (str, num) => {
     return (
@@ -125,21 +145,36 @@ const NoteCard = ({ note, user }) => {
           <div className="flex gap-5 mt-2 py-2">
             {/* Likes  */}
             <div className="flex gap-1 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 cursor-pointer"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-              <span className="text-sm font-bold">9 Likes</span>
+              {note?.likes.includes(session?.user?.id) ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 text-red-500 cursor-pointer"
+                  onClick={() => handelLike(false)}
+                >
+                  <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5 cursor-pointer"
+                  onClick={() => handelLike(true)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                  />
+                </svg>
+              )}
+              <span className="text-sm font-bold">
+                {note?.likes.length} Likes
+              </span>
             </div>
 
             {/* Comments */}
