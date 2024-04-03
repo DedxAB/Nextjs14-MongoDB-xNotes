8d9aad063cff33963f86c6240e56e1a9d1c15611
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-03 14:23:59 +0530

    Add newTags field to PATCH endpoint

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -3,12 +3,17 @@ import Topic from "@/models/topic.model";
 
 export async function PATCH(req, { params }) {
   const { id } = params;
-  const { newTitle: title, newDescription: description } = await req.json();
+  const {
+    newTitle: title,
+    newDescription: description,
+    newTags: tags,
+  } = await req.json();
   await connectDB();
   // Update the contentUpdatedAt field along with title and description for only the updated note
   await Topic.findByIdAndUpdate(id, {
     title,
     description,
+    tags,
     contentUpdatedAt: Date.now(),
   });
   return Response.json({ message: "Note updated" }, { status: 200 });
