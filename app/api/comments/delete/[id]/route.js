6a6d7ab538732c37a012f821ea0574ad191ab2cb
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-08 01:15:49 +0530

    Add route for deleting comments

---
+++ app/api/comments/delete/[id]/route.js
@@ -0,0 +1,35 @@
+import connectDB from "@/helper/mongodb";
+import Comment from "@/models/comment.model";
+import Note from "@/models/note.model";
+import { NextResponse } from "next/server";
+
+export const DELETE = async (req, { params }) => {
+  const { id: commentId } = params;
+  const { noteId } = await req.json();
+  try {
+    await connectDB();
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+    if (!deletedComment) {
+      return NextResponse.json(
+        { message: "Comment not found." },
+        { status: 404 }
+      );
+    }
+    await Note.findByIdAndUpdate(
+      noteId,
+      {
+        $pull: { comments: deletedComment._id },
+      },
+      { new: true }
+    );
+    return NextResponse.json(
+      { message: "Comment deleted successfully." },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to connect to the server." },
+      { status: 500 }
+    );
+  }
+};
