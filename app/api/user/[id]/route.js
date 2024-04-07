6e4e60afade028ef4e6a08fcf1fdfae280c22529
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-08 04:03:52 +0530

    Update user route to include social links

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -31,16 +31,20 @@ export const GET = async (_req, { params }) => {
 export const PATCH = async (req, { params }) => {
   await connectDB();
   const { id } = params;
-  const { bio } = await req.json();
+  const { bio, socialLinks } = await req.json();
   try {
-    const user = await User.findByIdAndUpdate(id, { bio }, { new: true });
+    const user = await User.findByIdAndUpdate(
+      id,
+      { bio, socialLinks },
+      { new: true }
+    );
     if (!user) {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
     return NextResponse.json({ message: "Bio updated" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { message: "Failed to make changes" },
+      { message: "Failed to connect to the server" },
       { status: 500 }
     );
   }
