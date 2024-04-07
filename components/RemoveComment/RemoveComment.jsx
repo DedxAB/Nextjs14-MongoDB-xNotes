df648b532176f8864fd486da5b5182d83697c6cd
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-07 16:16:06 +0530

    Add AlertDialog component for removing comments

--- components/RemoveComment/RemoveComment.jsx
+++ components/RemoveComment/RemoveComment.jsx
@@ -1,24 +1,83 @@
 "use client";
 import { Trash2 } from "lucide-react";
-import React from "react";
 import { Button } from "../ui/button";
 import { useSession } from "next-auth/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const RemoveComment = ({ comment, note }) => {
+  const [conformationMessage, setConformationMessage] = useState(false);
+  const router = useRouter();
   const { data: session } = useSession();
   //   console.log(comment);
   //   console.log(note);
+
+  const deleteComment = async () => {
+    if (conformationMessage) {
+      try {
+        toast.success("Comment Deleted Successfully.");
+        router.refresh();
+      } catch (e) {
+        toast.error("Failed to Delete Comment.");
+        console.log(e.message);
+      }
+      setConformationMessage(false);
+    }
+  };
+
   if (
     session?.user?.id === comment?.author?._id ||
     session?.user?.id === note?.author?._id
   ) {
     return (
       <>
-        <Button variant={`outline`} size="icon">
-          <Trash2 className="w-4 text-red-500" />
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              onClick={() => setConformationMessage(true)}
+              variant="outline"
+              size="icon"
+            >
+              <Trash2 className="w-4 text-red-500" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription className={`font-bold`}>
+                This action cannot be undone.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel
+                className={`font-bold`}
+                onClick={() => setConformationMessage(false)}
+              >
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction
+                className={`font-bold`}
+                onClick={deleteComment}
+              >
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </>
-    ); // return the remove comment button
+    );
   } else {
     return null;
   }
