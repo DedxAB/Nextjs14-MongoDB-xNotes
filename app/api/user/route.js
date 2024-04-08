c89b5e31355d14c4eb7d6622e0e9e33471b5c269
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-08 09:11:40 +0530

    Fix user creation and error handling

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -3,17 +3,26 @@ import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  await connectDB();
   const { email, name, image, username } = await req.json();
   try {
+    await connectDB();
     const isExist = await User.findOne({ email });
     if (isExist) {
       throw new Error("Email already exists");
     }
     const user = await User.create({ email, name, image, username });
+    if (!user) {
+      return NextResponse.json(
+        { message: "Failed to create user" },
+        { status: 400 }
+      );
+    }
     return NextResponse.json({ user }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
+    return NextResponse.json(
+      { message: "Failed to connect to server" },
+      { status: 500 }
+    );
   }
 }
 
