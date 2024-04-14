593c2c8a6bc70e9b4dd6950a5683f9c43e7a2737
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-15 04:00:36 +0530

    Refactor admin details page to include user profile and note feed

--- app/admin/[id]/details/page.jsx
+++ app/admin/[id]/details/page.jsx
@@ -1,5 +1,60 @@
-const page = () => {
-  return <>Admin Page</>;
+import NoteCard from "@/components/NoteCard/NoteCard";
+import ProfileSection from "@/components/ProfileSection/ProfileSection";
+import { BASE_URL } from "@/utils/constants";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Admin Profile",
+};
+
+const fetchUser = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/api/user/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "Failed to get User");
+    }
+    return await res.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const UserFeed = ({ notes, user }) => {
+  return (
+    <>
+      {notes && notes.length > 0 && (
+        <>
+          <hr className="my-6" />
+          <section>
+            <h1 className="font-bold text-lg mb-3">All Notes</h1>
+          </section>
+        </>
+      )}
+      {notes?.length > 0 &&
+        notes?.map((note) => {
+          return <NoteCard key={note?._id} note={note} user={user} />;
+        })}
+    </>
+  );
+};
+
+const page = async ({ params }) => {
+  const { id } = params;
+  const { user } = await fetchUser(id);
+
+  if (user?.isAdmin === false) {
+    redirect("/");
+  }
+
+  return (
+    <div className="min-h-[85vh]">
+      <ProfileSection user={user} />
+      <UserFeed notes={user?.notes} user={user} />
+    </div>
+  );
 };
 
 export default page;
