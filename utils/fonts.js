0ffb99d10d9b9559989cb9f1dc9d5ae4cd2e8af5
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-14 10:09:54 +0530

    Add Open Sans font to fonts.js

--- utils/fonts.js
+++ utils/fonts.js
@@ -3,8 +3,12 @@ import {
   Federo,
   Luckiest_Guy,
   Playfair_Display,
+  Open_Sans,
 } from "next/font/google";
 
+const opensans = Open_Sans({
+  subsets: ["latin"],
+});
 const federo = Federo({
   subsets: ["latin"],
   weight: ["400"],
@@ -26,5 +30,6 @@ const federo_font = federo.className;
 const luckiest_guy_font = luckiestGuy.className;
 const josefin_sans_font = josefinSans.className;
 const playfair_font = playfair.className;
+const opensans_font = opensans.className;
 
-export { josefin_sans_font, federo_font, luckiest_guy_font, playfair_font };
+export { josefin_sans_font, federo_font, luckiest_guy_font, playfair_font, opensans_font };
