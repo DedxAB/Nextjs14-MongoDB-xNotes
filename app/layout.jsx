828c9f2b9dd1ee85f2f44ae4dd9ac1dc370dec46
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-17 18:53:29 +0530

    Refactor layout component

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,6 @@ import { NextAuthProviders } from "./Providers";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
-
 export const metadata = {
   title: {
     default: "DedxNotes - Share Your Notes with the World",
@@ -34,7 +33,7 @@ export default function RootLayout({ children }) {
             <div className="w-full sticky top-0 left-0 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b z-10">
               <Navbar />
             </div>
-            <main className="max-w-3xl mx-auto px-4 mt-5">{children}</main>
+            <main className="max-w-3xl mx-auto px-4">{children}</main>
             <Toaster richColors position="bottom-right" />
             {/* footer  */}
             <div className="w-full border-t mt-8">
