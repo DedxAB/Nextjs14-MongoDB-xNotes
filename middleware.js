37bbad4149ddd3779a5488179be10e9a0691d4a8
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-19 08:58:49 +0530

    Update middleware.js to exclude "/api/:path*" from matcher

--- middleware.js
+++ middleware.js
@@ -2,16 +2,10 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     "/create-note",
     "/edit-note/:path*",
     "/admin/:path*", // Match /admin/username/details
     "/profile/:path*", // Match /profile/username/details
+    // "/api/:path*", // Match /api/endpoint
   ],
 };
