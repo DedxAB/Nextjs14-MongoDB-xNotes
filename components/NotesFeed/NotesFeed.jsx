d80859e3982cb93bad0d30a3f6e5ad567c09a84c
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-20 10:42:30 +0530

    Refactor fetchNotes function and remove console.log

--- components/NotesFeed/NotesFeed.jsx
+++ components/NotesFeed/NotesFeed.jsx
@@ -4,7 +4,7 @@ import NoteCard from "../NoteCard/NoteCard";
 const fetchNotes = async () => {
   try {
     const res = await fetch(`${BASE_URL}/api/notes`, {
-      cache: "no-store", // Disable cache
+      cache: "no-store",
     });
     if (!res.ok) {
       const errorData = await res.json();
@@ -19,34 +19,10 @@ const NotesFeed = async () => {
   // Fetch the notes
   const { notes } = await fetchNotes();
 
-  /* 
-  {
-    _id: '65fd82b743f93605c6ecc5a4',
-    title: 'Tailwind Gradient',
-    description: 'TAILWIND CSS GRADIENT GENERATOR.\n' +
-      'TailwindGradient.com is a user-friendly web tool designed to mix Tailwind CSS Colors and generate stunning text and background allowing you to achieve the perfect colors. you can easily copy and paste the class names.',
-    author: {
-      _id: '65fd620f650dea0c24bd294d',
-      email: 'hawtsauce.signin@gmail.com',
-      name: 'Hawt Sauce',
-      image: 'https://lh3.googleusercontent.com/a/ACg8ocIRxM9_MlMj8YWfXmuEZdMPoT10ivNvqW6HeOWMbNvAGg=s96-c',
-      username: 'hawtsauce.signin',
-      createdAt: '2024-03-22T10:48:47.962Z',
-      updatedAt: '2024-03-27T09:37:44.320Z',
-      __v: 0,
-      notes: [Array]
-    },
-    createdAt: '2024-03-22T13:08:07.545Z',
-    updatedAt: '2024-03-23T15:55:24.030Z',
-    __v: 0
-  }
-  */
-
   return (
     <>
       {/* Show the note card */}
       {notes?.map((note) => {
-        // console.log(note);
         return <NoteCard key={note?._id} note={note} user={note?.author} />;
       })}
     </>
