a6f9305dbf96a23832b9261ee58d813c7d257751
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-21 00:40:11 +0530

    Update error component styling

--- app/error.jsx
+++ app/error.jsx
@@ -6,9 +6,9 @@ import { RotateCcw } from "lucide-react";
 const Error = () => {
   return (
     <div className="flex h-[85vh] items-center justify-center">
-      <div className="my-auto flex flex-wrap items-center justify-center gap-3 text-lg md:text-2xl font-bold">
+      <div className="my-auto flex flex-wrap items-center justify-center space-x-3 text-lg md:text-2xl font-bold">
         <p className="text-center">
-          <span className="bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent mr-2">
+          <span className="mr-2">
             Opps!
           </span>
           Something went wrong.
@@ -18,7 +18,7 @@ const Error = () => {
           onClick={() => window.location.reload(false)}
           className={`text-lg md:text-xl font-bold md:py-[19px]`}
         >
-          <RotateCcw className="w-[1.2rem] mr-1" />
+          <RotateCcw className="w-[1.2rem] h-[1.2rem] mr-1" />
           Retry
         </Button>
       </div>
