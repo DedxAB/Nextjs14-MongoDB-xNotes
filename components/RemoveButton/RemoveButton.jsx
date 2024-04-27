fae75ab3999ea11c6a9bf97422c89a25a05f6e69
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-27 10:38:06 +0530

    Refactor RemoveButton component and add toast loading message

--- components/RemoveButton/RemoveButton.jsx
+++ components/RemoveButton/RemoveButton.jsx
@@ -16,8 +16,9 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "../ui/alert-dialog";
+import { source_code_pro_font } from "@/utils/fonts";
 
-const RemoveButton = ({ id }) => {
+const RemoveButton = ({ id, title }) => {
   const [conformationMessage, setConformationMessage] = useState(false);
 
   const router = useRouter();
@@ -25,6 +26,7 @@ const RemoveButton = ({ id }) => {
   const removeNote = async () => {
     if (conformationMessage) {
       try {
+        const toastId = toast.loading("Deleting Note...");
         const res = await fetch(`/api/notes?id=${id}`, {
           method: "DELETE",
         });
@@ -33,7 +35,9 @@ const RemoveButton = ({ id }) => {
           const errorData = await res.json();
           throw new Error(errorData.message || "Error deleting note");
         }
-        toast.success("Note Deleted Successfully.");
+        toast.success("Note Deleted Successfully.", {
+          id: toastId,
+        });
         router.refresh();
       } catch (e) {
         toast.error(e.message);
@@ -59,7 +63,10 @@ const RemoveButton = ({ id }) => {
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription className={`font-bold`}>
-              This action cannot be undone.
+              This action cannot be undone. You are about to delete this note :{" "}
+              <span className={`text-primary ${source_code_pro_font}`}>
+                {title}
+              </span>
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
