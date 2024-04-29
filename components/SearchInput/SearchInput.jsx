14b3b3e1aa365f171c833afbaaa5474eaded7f42
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-29 13:54:33 +0530

    Add dynamic placeholder to SearchInput component

--- components/SearchInput/SearchInput.jsx
+++ components/SearchInput/SearchInput.jsx
@@ -1,10 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import { useRouter } from "next/navigation";
 
 const SearchInput = () => {
   const [search, setSearch] = useState("");
+  const [placeholder, setPlaceholder] = useState("Search notes...");
+
+  const searchTerms = [
+    "Search notes...",
+    "Search keyword...",
+    "Search username...",
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * searchTerms.length);
+      setPlaceholder(searchTerms[randomIndex]);
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  },[]); 
 
   const router = useRouter();
   const handelSubmit = (e) => {
@@ -20,7 +36,7 @@ const SearchInput = () => {
             onChange={(e) => setSearch(e.target.value)}
             name="search"
             type="text"
-            placeholder="Search notes, keyword, or username..."
+            placeholder={placeholder}
             className="w-full md:w-1/2 font-bold px-4 py-5"
           />
         </form>
