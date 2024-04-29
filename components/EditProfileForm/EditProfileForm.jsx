870ef6e6547f3940c2d77a54432e3af51d4ebb0a
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-29 13:54:59 +0530

    Add banner to EditProfileForm component

--- components/EditProfileForm/EditProfileForm.jsx
+++ components/EditProfileForm/EditProfileForm.jsx
@@ -88,6 +88,7 @@ const EditProfileForm = ({ userId, bio, socialLinks }) => {
 
   return (
     <>
+      {/* Banner */}
       <section className="font-bold text-[#444746] mb-6 mt-8">
         <h1 className="text-4xl md:text-5xl py-1 bg-gradient-to-r from-blue-500  via-red-500 to-pink-500 bg-clip-text text-transparent">
           Profile details changes?
