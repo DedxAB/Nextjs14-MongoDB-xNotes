23266720146fecd37f47af0d5a96cff36e9dedc4
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-29 12:45:06 +0530

    Refactor fetchData function and import fetchResultOfQuery from resultServices

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,48 +1,17 @@
 import SearchFeed from "@/components/SearchFeed/SearchFeed";
 import SearchResultBanner from "@/components/SearchResultBanner/SearchResultBanner";
-import { BASE_URL } from "@/utils/constants";
+import { fetchResultOfQuery } from "@/services/resultServices";
 
 export const metadata = {
   title: "Result",
 };
 
-const fetchData = async (q) => {
-  try {
-    const notesResponse = fetch(`${BASE_URL}/api/results/notes/${q}`, {
-      cache: "no-store",
-    });
-    const usersResponse = fetch(`${BASE_URL}/api/results/users/${q}`, {
-      cache: "no-store",
-    });
-
-    const [dataNotes, dataUsers] = await Promise.all([
-      notesResponse,
-      usersResponse,
-    ]);
-
-    if (!dataNotes.ok || !dataUsers.ok) {
-      const errorNotesData = await dataNotes.json();
-      const errorUsersData = await dataUsers.json();
-      throw new Error(
-        errorNotesData.message ||
-          errorUsersData.message ||
-          "Something went wrong!"
-      );
-    }
-
-    const notesArray = await dataNotes.json();
-    const usersArray = await dataUsers.json();
-
-    return { notesArray, usersArray };
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 const Result = async ({ searchParams }) => {
   const { q } = searchParams;
 
-  const { notesArray, usersArray } = await fetchData(encodeURIComponent(q));
+  const { notesArray, usersArray } = await fetchResultOfQuery(
+    encodeURIComponent(q)
+  );
   const notes = notesArray;
   //   console.log("notes", notes);
   const users = usersArray;
