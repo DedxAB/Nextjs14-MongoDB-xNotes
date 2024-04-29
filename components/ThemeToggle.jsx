cff0da9001917bb8e6c8048e6e3ef6c51bd06d67
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-04-30 03:26:21 +0530

    Refactor theme toggle icons

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -19,8 +19,8 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon className="w-4 h-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute w-4 h-4 rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-100" />
+          <SunIcon className="w-4 h-4 scale-100 transition-all dark:scale-0" />
+          <MoonIcon className="absolute w-4 h-4 scale-0 transition-all dark:rotate-90 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
