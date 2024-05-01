a14c266bc47a31ae2a2c4dc56363e2192e1fa327
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-02 02:10:17 +0530

    Refactor user details page to use AllUserDetails component

--- app/admin/[id]/details/all-user/page.jsx
+++ app/admin/[id]/details/all-user/page.jsx
@@ -1,9 +1,6 @@
-import AdminToggle from "@/components/AdminToggle/AdminToggle";
-import UserCard from "@/components/UserCard/UserCard";
+import AllUserDetails from "@/components/AllUserDetails/AllUserDetails";
 import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
-import { Input } from "@/components/ui/input";
 import { fetchAllUser, fetchUserByEmail } from "@/services/userServices";
-import { josefin_sans_font } from "@/utils/fonts";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
@@ -27,27 +24,9 @@ const page = async () => {
         title={`Welcome Admin`}
         description={`Here you can see all the users and toggle their admin status.`}
       />
-      {/* Search the user by name */}
-      <form className="my-5 w-full md:w-1/2">
-        <Input placeholder={`Search by name`} className={`font-bold`}></Input>
-      </form>
-      <h1 className="font-bold text-lg my-5">All Users</h1>
-      {allUsers?.length > 0 &&
-        allUsers?.map((user) => {
-          return (
-            <div className="w-full relative" key={user?._id}>
-              <UserCard user={user} />
-              <div
-                className={`px-3 md:px-4 py-1 absolute right-0 bottom-1 ${josefin_sans_font} text-xs`}
-              >
-                <AdminToggle
-                  user={user}
-                  currentUserEmail={session?.user?.email}
-                />
-              </div>
-            </div>
-          );
-        })}
+
+      {/* All Users details  */}
+      <AllUserDetails allUsers={allUsers} session={session} />
     </div>
   );
 };
