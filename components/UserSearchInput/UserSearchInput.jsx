da86500d9b24ab3700782d62d540aaca4e480942
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-02 02:11:19 +0530

    Add UserSearchInput component

---
+++ components/UserSearchInput/UserSearchInput.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+
+export default function UserSearchInput({ allUsers, setFilteredUsers }) {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (searchText.trim() === "") {
+        setFilteredUsers(allUsers);
+      } else {
+        const filtered = allUsers.filter(
+          (user) =>
+            user?.name?.toLowerCase().includes(searchText.toLowerCase()) ||
+            user?.username?.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setFilteredUsers(filtered);
+      }
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [allUsers, searchText, setFilteredUsers]);
+
+  return (
+    <>
+      <Input
+        placeholder={`Search user by name or username`}
+        className={`font-bold my-5 w-full md:w-1/2`}
+        onChange={(e) => setSearchText(e.target.value)}
+        value={searchText}
+      />
+    </>
+  );
+}
