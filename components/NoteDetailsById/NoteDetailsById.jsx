f9562866d3d5d6a35fa8f11d379381c6778fe5ed
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-02 14:25:05 +0530

    Add WelcomeBanner component to NoteDetailsById.jsx

--- components/NoteDetailsById/NoteDetailsById.jsx
+++ components/NoteDetailsById/NoteDetailsById.jsx
@@ -1,6 +1,7 @@
 import NoteCard from "../NoteCard/NoteCard";
 import NoteCommentCard from "../NoteCommentCard/NoteCommentCard";
 import NoteCommentForm from "../NoteCommentForm/NoteCommentForm";
+import WelcomeBanner from "../WelcomeBanner/WelcomeBanner";
 
 const NoteDetailsById = ({ note }) => {
   const user = note?.author;
@@ -8,12 +9,10 @@ const NoteDetailsById = ({ note }) => {
   return (
     <>
       {/* Create Note banner  */}
-      <div className="font-bold text-[#444746] mb-6 mt-8">
-        <h1 className="text-4xl md:text-5xl py-1 bg-gradient-to-r from-blue-500  via-red-500 to-pink-500 bg-clip-text text-transparent">
-          Note Details
-        </h1>
-        <h1 className="text-xl md:text-2xl my-2">Feel free to comment.</h1>
-      </div>
+      <WelcomeBanner
+        title={`Note Details`}
+        description={`Feel free to comment.`}
+      />
 
       <NoteCard note={note} user={user} />
       <NoteCommentForm note={note} />
