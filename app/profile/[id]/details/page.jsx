9a15583ae5b960a758b71d6dafb8de735b708ea0
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-03 13:07:48 +0530

    Add session authentication and filter notes based on visibility

--- app/profile/[id]/details/page.jsx
+++ app/profile/[id]/details/page.jsx
@@ -1,6 +1,7 @@
 import ProfileSection from "@/components/ProfileSection/ProfileSection";
 import UserFeed from "@/components/UserNotesFeed/UserNotesFeed";
 import { fetchUserById } from "@/services/userServices";
+import { getServerSession } from "next-auth";
 
 export const generateMetadata = async ({ params }) => {
   const { id } = params;
@@ -13,12 +14,18 @@ export const generateMetadata = async ({ params }) => {
 const Profile = async ({ params }) => {
   const { id } = params;
   const { user } = await fetchUserById(id);
+  const session = await getServerSession();
+
+  const isCurrentUserPrifile = session?.user?.email === user?.email;
+  const filteredNotes = user?.notes.filter((note) => {
+    return isCurrentUserPrifile || note?.visibility === "public";
+  });
 
   return (
     <div className="min-h-[85vh]">
       <ProfileSection user={user} />
       <hr className="my-6" />
-      <UserFeed notes={user?.notes} user={user} />
+      <UserFeed notes={filteredNotes} user={user} />
     </div>
   );
 };
--- components/NotesFeed/NotesFeed.jsx
+++ components/NotesFeed/NotesFeed.jsx
@@ -1,14 +1,23 @@
+import { getServerSession } from "next-auth";
 import NoteCard from "../NoteCard/NoteCard";
 import { fetchAllNotes } from "@/services/noteServices";
 
 const NotesFeed = async () => {
   // Fetch the all notes
   const { notes } = await fetchAllNotes();
+  const session = await getServerSession();
+
+  const currentUserEmail = session?.user?.email;
+  const filteredNotes = notes?.filter((note) => {
+    return (
+      currentUserEmail === note?.author?.email || note?.visibility === "public"
+    );
+  });
 
   return (
     <>
       {/* Show the note card */}
-      {notes?.map((note) => {
+      {filteredNotes?.map((note) => {
         return <NoteCard key={note?._id} note={note} user={note?.author} />;
       })}
     </>
