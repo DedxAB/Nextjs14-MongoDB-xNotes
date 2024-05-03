98c1e7755f85029d19aef0ae73f88e7d18457bf8
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-03 13:17:51 +0530

    Add server-side authentication to Result component

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,6 +1,7 @@
 import SearchFeed from "@/components/SearchFeed/SearchFeed";
 import SearchResultBanner from "@/components/SearchResultBanner/SearchResultBanner";
 import { fetchResultOfQuery } from "@/services/resultServices";
+import { getServerSession } from "next-auth";
 
 export const generateMetadata = async ({ searchParams }) => {
   const { q } = searchParams;
@@ -11,18 +12,25 @@ export const generateMetadata = async ({ searchParams }) => {
 
 const Result = async ({ searchParams }) => {
   const { q } = searchParams;
+  const session = await getServerSession();
+  const currentUserEmail = session?.user?.email;
 
   const { notesArray, usersArray } = await fetchResultOfQuery(
     encodeURIComponent(q)
   );
   const notes = notesArray;
   //   console.log("notes", notes);
+  const filteredNotes = notes?.filter((note) => {
+    return (
+      currentUserEmail === note?.author?.email || note?.visibility === "public"
+    );
+  });
   const users = usersArray;
   //   console.log("users", users);
   return (
     <div className="min-h-[85vh]">
       <SearchResultBanner searchingFor={q} notes={notes} users={users} />
-      <SearchFeed notes={notes} users={users} />
+      <SearchFeed notes={filteredNotes} users={users} />
     </div>
   );
 };
