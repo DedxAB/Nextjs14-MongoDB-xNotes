21bffe1730af24eebc1464018e36ad33bdb46e24
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-05-05 16:39:15 +0530

    Update section padding in FeatureSection component

--- components/FeatureSection/FeatureSection.jsx
+++ components/FeatureSection/FeatureSection.jsx
@@ -1,6 +1,6 @@
 const FeatureSection = () => {
   return (
-    <section className="py-16">
+    <section className="py-10">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Authentication */}
