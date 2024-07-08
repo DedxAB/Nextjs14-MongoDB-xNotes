0927e35c2f3e42b9b92e9556aa13c2c8db50b5a1
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-09 01:19:18 +0530

    feat: Add usePathname hook to BottomNavbar component

--- components/BottomNavbar/BottomNavbar.jsx
+++ components/BottomNavbar/BottomNavbar.jsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 import { useSession } from "next-auth/react";
 import { NotebookPen, Search, X } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import ShowSearch from "../ShowSearch/ShowSearch";
+import { cn } from "@/lib/utils";
 
 export default function BottomNavbar() {
   const [openSearch, setOpenSearch] = useState(false);
   const [searchText, setSearchText] = useState("");
   const router = useRouter();
   const { status, data: session } = useSession();
+  const pathName = usePathname();
 
   useEffect(() => {
     let timeoutId;
@@ -42,23 +44,47 @@ export default function BottomNavbar() {
   return (
     <div className="max-w-3xl px-4 py-3">
       <div className="flex justify-between items-center gap-3">
-        <Link href={`/`}>
-          <Button className={`rounded-full`} variant={`outline`}>
-            Home
-          </Button>
+        <Link
+          href={`/`}
+          className={cn(
+            `flex items-center justify-between gap-1 transition-all duration-300 ease-in-out border hover:border-primary rounded-full py-3 px-3 h-9`,
+            pathName === "/" && "border-primary"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-5 h-5"
+            fill={"none"}
+          >
+            <path
+              d="M9.06165 4.82633L3.23911 9.92134C2.7398 10.3583 3.07458 11.1343 3.76238 11.1343C4.18259 11.1343 4.52324 11.4489 4.52324 11.8371V15.0806C4.52324 17.871 4.52324 19.2662 5.46176 20.1331C6.40029 21 7.91082 21 10.9319 21H13.0681C16.0892 21 17.5997 21 18.5382 20.1331C19.4768 19.2662 19.4768 17.871 19.4768 15.0806V11.8371C19.4768 11.4489 19.8174 11.1343 20.2376 11.1343C20.9254 11.1343 21.2602 10.3583 20.7609 9.92134L14.9383 4.82633C13.5469 3.60878 12.8512 3 12 3C11.1488 3 10.4531 3.60878 9.06165 4.82633Z"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M12 16H12.009"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span>Home</span>
         </Link>
         <div className="flex items-center justify-between gap-2">
-          <Button
-            className={`flex items-center gap-1 rounded-full`}
-            variant="outline"
+          <div
+            className={`flex items-center justify-between gap-1 transition-all duration-300 ease-in-out border hover:border-primary rounded-full py-2 px-3 h-9 cursor-pointer`}
             onClick={() => setOpenSearch(!openSearch)}
           >
             <Search className="w-5 h-5" />
             <span>Search</span>
-          </Button>
+          </div>
           {status === "authenticated" && (
             <Link href={`/create-note`}>
-              <Button className="font-bold md:hidden flex items-center justify-between rounded-full gap-1">
+              <Button className="font-bold flex items-center justify-between gap-1 rounded-full">
                 <NotebookPen className="w-4 h-4" />
                 <span>Write</span>
               </Button>
