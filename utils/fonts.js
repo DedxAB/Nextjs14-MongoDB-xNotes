ff4936f25a62677a119b2064435cf4e5f7b34bf6
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-08 23:14:45 +0530

    refactor: Update fonts.js to include Poppins font and its variants

--- utils/fonts.js
+++ utils/fonts.js
@@ -4,7 +4,13 @@ import {
   Open_Sans,
   Inter,
   Source_Code_Pro,
+  Poppins,
 } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight:["400", "500", "600", "700"]
+})
 
 const opensans = Open_Sans({
   subsets: ["latin"],
@@ -32,6 +38,7 @@ const playfair_font = playfair.className;
 const opensans_font = opensans.className;
 const inter_font = inter.className;
 const source_code_pro_font = sourceCodePro.className;
+const poppins_font = poppins.className;
 
 export {
   josefin_sans_font,
@@ -39,4 +46,5 @@ export {
   opensans_font,
   inter_font,
   source_code_pro_font,
+  poppins_font,
 };
