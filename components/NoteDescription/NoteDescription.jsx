730fa1dfc63df2569ac8df0254f627349f70fdea
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-08 23:14:09 +0530

    Refactor NoteDescription component and remove commented code

--- components/NoteDescription/NoteDescription.jsx
+++ components/NoteDescription/NoteDescription.jsx
@@ -7,32 +7,6 @@ import ReactLinkify from "react-linkify";
 const NoteDescription = ({ description, noteId = "" }) => {
   const pathName = usePathname();
 
-  //   const highlightLink = () => {
-  //     const regex =
-  //       /https?:\/\/[-a-zA-Z0-9@:%._\+~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-
-  //     // Split the description by space to check each word individually
-  //     return description.split(" ").map((word, index) => {
-  //       // If the word matches the URL regex, render it as a link
-  //       if (word.match(regex)) {
-  //         return (
-  //           <a
-  //             key={index}
-  //             href={word}
-  //             target="_blank"
-  //             rel="noopener noreferrer"
-  //             style={{ color: "blue", textDecoration: "underline" }}
-  //           >
-  //             {word}
-  //           </a>
-  //         );
-  //       } else {
-  //         // If it's not a URL, return the word as is
-  //         return word + " ";
-  //       }
-  //     });
-  //   };
-
   const truncateString = (str, num) => {
     return (
       <>
@@ -70,7 +44,9 @@ const NoteDescription = ({ description, noteId = "" }) => {
         className={`text-sm md:text-base mt-[0.28rem] py-1 whitespace-pre-line ${inter_font}`}
       >
         <ReactLinkify componentDecorator={customLink}>
-          {pathName === `/note/${noteId}/details` || pathName === "/create-note"
+          {pathName === `/note/${noteId}/details` ||
+          pathName === "/create-note" ||
+          pathName === `/edit-note/${noteId}`
             ? description
             : truncateString(description, 140)}
         </ReactLinkify>
