f77a65176f8f4beb77ecc2c707f6c09fe818e891
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-12 17:18:47 +0530

    feat: Fetch and display notifications for the current user in AllNotification component

--- components/Notification/AllNotification.jsx
+++ components/Notification/AllNotification.jsx
@@ -1,3 +1,13 @@
-export default function AllNotification() {
+import { fetchNotificationsByUserId } from "@/services/notificationServices";
+import { fetchUserByEmail } from "@/services/userServices";
+import { getServerSession } from "next-auth";
+
+export default async function AllNotification() {
+  const session = await getServerSession();
+  const currentUser = await fetchUserByEmail(session?.user?.email);
+  //   console.log(currentUser.user._id);
+  const { data: notifications } = await fetchNotificationsByUserId(
+    currentUser?.user?._id
+  );
   return <div>AllNotification</div>;
 }
