d249157c9e7fd9055bcfe5839b7222c25c15e110
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-13 06:37:01 +0530

    refactor: Update notification route to handle PATCH requests

---
+++ app/api/notifications/[notificationId]/route.js
@@ -0,0 +1,31 @@
+import connectDB from "@/db/mongodb";
+import Notification from "@/models/notification.model";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req, { params }) {
+  const { notificationId } = params;
+  const { isRead } = await req.json();
+  try {
+    await connectDB();
+    const notification = await Notification.findByIdAndUpdate(
+      notificationId,
+      { isRead },
+      { new: true }
+    );
+    if (!notification) {
+      return NextResponse.json(
+        { message: "Notification not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      { message: "Notification updated", data: notification },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to connect with the server" },
+      { status: 500 }
+    );
+  }
+}
