62d9c1102b9ce1e6290d5b447cf8a4cd07c23455
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-07-13 06:49:22 +0530

    refactor: Fetch and display notifications for the current user in AllNotification component

--- app/notifications/page.jsx
+++ app/notifications/page.jsx
@@ -1,7 +1,14 @@
 import AllNotification from "@/components/Notification/AllNotification";
 import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
+import { fetchNotificationsByUserId } from "@/services/notificationServices";
+import { fetchUserByEmail } from "@/services/userServices";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession();
+  const currentUser = await fetchUserByEmail(session?.user?.email);
+  const data = await fetchNotificationsByUserId(currentUser?.user?._id);
+  const notifications = data.data ? data.data : [];
   return (
     <>
       <section id="banner-notification">
@@ -12,7 +19,7 @@ export default function page() {
       </section>
 
       <section id="all-notification">
-        <AllNotification />
+        <AllNotification notifications={notifications} />
       </section>
     </>
   );
