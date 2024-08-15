40792620f25ab5346d831286aeb8a4f5a4c91b10
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-08-15 22:16:30 +0530

    refactor: Add SavedNotesContext and SavedNotesProvider for managing saved notes

---
+++ context/SavedNotesContext.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { handleCheckAllSavedNotes } from "@/actions/note.actions";
+
+const SavedNotesContext = createContext();
+
+export const SavedNotesProvider = ({ children }) => {
+  const { data: session } = useSession();
+  const [savedNotesMap, setSavedNotesMap] = useState({});
+
+  useEffect(() => {
+    const fetchSavedNotes = async () => {
+      if (session?.user?.id) {
+        const savedNotes = await handleCheckAllSavedNotes(session.user.id);
+        setSavedNotesMap(savedNotes);
+      }
+    };
+
+    fetchSavedNotes();
+  }, [session?.user?.id]);
+
+  return (
+    <SavedNotesContext.Provider value={{ savedNotesMap, setSavedNotesMap }}>
+      {children}
+    </SavedNotesContext.Provider>
+  );
+};
+
+export const useSavedNotes = () => useContext(SavedNotesContext);
