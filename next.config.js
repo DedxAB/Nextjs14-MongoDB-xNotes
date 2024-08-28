914b9dac1f37c8453452a1c7370d5ebb3329b9f1
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-08-28 23:52:57 +0530

    refactor: Add next-pwa for progressive web app support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -14,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
