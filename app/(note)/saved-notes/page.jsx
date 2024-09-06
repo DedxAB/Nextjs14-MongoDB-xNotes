98ddb28fb0b06ed8ddf976441670c7c97e407f27
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-09-07 04:01:36 +0530

    refactor: Add saved notes page component and fetch saved notes by user ID

---
+++ app/(note)/saved-notes/page.jsx
@@ -0,0 +1,44 @@
+import NoteCard from "@/components/NoteCard/NoteCard";
+import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
+import { fetchAllSavedNotesByUserId } from "@/services/noteServices";
+import { fetchUserByEmail } from "@/services/userServices";
+import { getServerSession } from "next-auth";
+
+export default async function page() {
+  const session = await getServerSession();
+  const currentUser = await fetchUserByEmail(session?.user?.email);
+  const data = await fetchAllSavedNotesByUserId(currentUser?.user?._id);
+  const { data: savedNotes = [] } = data ?? {};
+  return (
+    <>
+      <section id="savednote-banner">
+        <WelcomeBanner
+          title="Welcome Back!"
+          description={
+            savedNotes.length > 0
+              ? `You've saved ${savedNotes.length} ${
+                  savedNotes.length === 1 ? "note" : "notes"
+                }. Explore ${savedNotes.length === 1 ? "it" : "them"} below.`
+              : "You haven't saved any notes yet."
+          }
+        />
+      </section>
+
+      {savedNotes.length < 1 && (
+        <h1 className="font-bold text-base">Start by adding your first one!</h1>
+      )}
+
+      {savedNotes.length > 0 &&
+        savedNotes.map(
+          (note) =>
+            !!note?.noteId && (
+              <NoteCard
+                key={note._id}
+                note={note.noteId}
+                noteAuthor={note.noteId.author}
+              />
+            )
+        )}
+    </>
+  );
+}
