981ce3d43b7f262d121554316786f8ef07423ccc
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-09-10 16:30:02 +0530

    refactor: Add lazy loading for images in user notifications route

--- app/api/notifications/user/[userId]/route.js
+++ app/api/notifications/user/[userId]/route.js
@@ -1,13 +1,31 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import connectDB from "@/db/mongodb";
 import Note from "@/models/note.model";
 import Notification from "@/models/notification.model";
 import User from "@/models/user.model";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export const GET = async (_req, { params }) => {
-  const { userId } = params;
   try {
     await connectDB();
+    const { userId } = params;
+
+    // check if the user is the current logged in user or not
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json(
+        { error: "You need to login first" },
+        { status: 401 }
+      );
+    }
+
+    // check if the user is the current logged in user or not
+    if (userId !== session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
     const notifications = await Notification.find({
       $or: [{ noteOwnerId: userId }, { type: "admin" }],
     })
@@ -18,17 +36,17 @@ export const GET = async (_req, { params }) => {
 
     if (!notifications) {
       return NextResponse.json(
-        { message: "Notifications not found" },
+        { error: "Notifications not found" },
         { status: 404 }
       );
     }
     return NextResponse.json(
-      { data: notifications, message: "Notifications fetched successfully" },
+      { message: "Success", data: notifications },
       { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
-      { message: "Failed to connect to the database" },
+      { error: "Failed to connect to the database" },
       { status: 500 }
     );
   }
